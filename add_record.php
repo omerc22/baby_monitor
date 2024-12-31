af17528c8965c51e0cd942f46b6dd0b54a8edf66
lvdd <omercelk3422gmail.com>
2025-01-01 00:12:05 +0300

    new year new commit

--- add_record.php
+++ add_record.php
@@ -1,31 +1,32 @@
 <?php
-$servername = "localhost";
-$username = "root"; // MySQL kullanıcı adı
-$password = ""; // MySQL şifreniz
-$dbname = "baby_monitor";
+include 'db_connection.php';
 
-// POST ile gelen verileri alın
-$temperature = $_POST['temperature'];
-$humidity = $_POST['humidity'];
-$sound_level = $_POST['sound_level'];
-$alert = $_POST['alert'];
+// POST ile gelen verileri alın, önce var olup olmadıklarını kontrol et
+if (isset($_POST['temperature']) && isset($_POST['humidity']) && isset($_POST['sound_level']) && isset($_POST['alert'])) {
+    $temperature = $_POST['temperature'];
+    $humidity = $_POST['humidity'];
+    $sound_level = $_POST['sound_level'];
+    $alert = $_POST['alert'];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Bağlantıyı kontrol edin
-if ($conn->connect_error) {
-    die("Bağlantı hatası: " . $conn->connect_error);
-}
+    // Bağlantıyı kontrol edin
+    if ($conn->connect_error) {
+        die("Bağlantı hatası: " . $conn->connect_error);
+    }
 
-// Veriyi ekle
-$sql = "INSERT INTO records (temperature, humidity, sound_level, alert) 
-        VALUES ('$temperature', '$humidity', '$sound_level', '$alert')";
+    // Veriyi ekle
+    $sql = "INSERT INTO records (temperature, humidity, sound_level, alert) 
+            VALUES ('$temperature', '$humidity', '$sound_level', '$alert')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Veri başarıyla eklendi";
+    if ($conn->query($sql) === TRUE) {
+        echo "Veri başarıyla eklendi";
+    } else {
+        echo "Hata: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 } else {
-    echo "Hata: " . $sql . "<br>" . $conn->error;
+    echo "Eksik veri gönderildi.";
 }
-
-$conn->close();
 ?>
